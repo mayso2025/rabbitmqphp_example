9974a889897e6b4f5f86a9a22c3687bf41972a8a
yumis56 <ys56@njit.edu>
2024-10-30 17:21:36 -0400

    might be changing idea

--- travelData.php
+++ travelData.php
@@ -12,11 +12,23 @@ $clientSecret = $_ENV['API_SECRET'];
 
 function getAccessToken($clientId, $clientSecret) {
 	$url = 'https://test.api.amadeus.com/v1/security/oauth2/token';
-	$data = [
+	$headers = [
 		'grant_type' => 'client_credentials',
 		'client_id' => $clientId,
 		'client_secret'=$clientSecret,
 	];
+
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch,CURLOPT_POSTFIELDS http_build_query($headers));
+
+	$response = curl_exec($ch);
+	curl_close($ch);
+
+	$responseData= json_decode($response,true);
+	return $responseData['access_token'];
+
 }
 
 

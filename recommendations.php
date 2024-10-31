0f4c0c1f719fb4e7d0610fd2b461bfec4bda7063
Mike Gabriel Ayson <maa57@njit.edu>
2024-10-31 05:28:32 -0400

    Added the barebones of the recommendations feature

---
+++ recommendations.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html> 
+<!-- THE ACTUAL FILE IS NOT TO BE RUN OR CALLED, IT IS SIMPLY A STAGING GROUND FOR WHERE WE NEED TO TEST NEW FEAUTRES AND THEN COPY THEM INTO welcome.php -->
+    <body> 
+        <p> Choose your preferred budget </p>
+    <form> <!-- Code gotten from w3schools https://www.w3schools.com/html/html_form_input_types.asp -->
+        <input type="radio" id="cheap" name="cheapOption" value="Cheap">
+        <label for="cheapOption">Cheaper Spots</label><br>
+        <input type="radio" id="vehicle2" name="expensiveOption" value="Expensive">
+        <label for="expensiveOption">Expensive Spots</label><br>
+    </form>
+    <br>
+    <p> Choose your preferred spaces</p>
+    <form> <!-- Code gotten from w3schools https://www.w3schools.com/html/html_form_input_types.asp -->
+        <input type="checkbox" id="cheap" name="cheapOption" value="Cheap">
+        <label for="cheapOption">Parks</label><br>
+        <input type="checkbox" id="vehicle2" name="vehicle2" value="Expensive">
+        <label for="checkbox">Resturants</label><br>
+        <input type="checkbox" id="vehicle2" name="vehicle2" value="Expensive">
+        <label for="checkbox">Shopping Stores</label><br>
+        <input type="checkbox" id="vehicle2" name="vehicle2" value="Expensive">
+        <label for="checkbox">Tourist Spots</label><br>
+    </form>
+    </body> 
+    <br>
+    <div> 
+        <header> Here are some spots we feel you'd like! </header> 
+    <!-- Insert the code that is needed to display the locations and their corresponding images-->
+    <!-- Utilize the usage of HTML Cards https://www.w3schools.com/howto/howto_css_cards.asp --> 
+    </div>
+</html>

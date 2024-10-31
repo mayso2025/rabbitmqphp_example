d553e1076bfd16c32e8b25b145c3476a4c61b30d
pd438 <pd438@njit.edu>
2024-10-31 01:08:40 -0400

    patch notes

--- booking.php
+++ booking.php
@@ -11,18 +11,18 @@ if (isset($_SESSION['message'])){
 ?>
 <form action= "booking-handler.php" method="post" enctype="multipart/form-data">
         <label for="guestName">Name of booker</label> <br> 
-        <input type="text"> <br> 
+        <input type="text" name="guestName"> <br> 
         <label for="numGuest"> How many people in total? </label><br> 
-        <input type="number" min="1"> <br> 
+        <input type="number" name="numGuest" min="1"> <br> 
         <label for="checkinDate"> What is expected check in date? </label><br> 
-        <input type="date"> <br> 
+        <input type="date" name="checkinDate"> <br> 
         <label for="checkinTime"> What is your expected check in time? </label><br> 
-        <input type="time"> <br> 
+        <input type="time" name="checkinTime"> <br> 
         <label for="checkoutDate"> When is your expected check out date </label><br> 
-        <input type="date"> <br> 
+        <input type="date" name="checkoutDate"> <br> 
         <label for="checkoutTime"> When is your expected check out time </label><br> 
-        <input type="time"> <br> 
+        <input type="time" name="checkoutTime"> <br> 
         <label for="location"> Which hotel will you be booking? </label><br> 
-        <input type="text"> <br> 
+        <input type="text" name="location"> <br> 
       </form>
 </html>

6dee343127d1747e59f20e4d7cf1aa854cfa362e
yumis56 <ys56@njit.edu>
2024-10-31 05:03:14 -0400

    FIXED! no need for role, just do username

--- welcome.php
+++ welcome.php
@@ -5,9 +5,9 @@ require_once('rabbitMQLib.inc');
 session_start();
 
 // Check if the user is logged in by verifying if the session variables are set
-/* TODO session is not persisting, may be due to localhost or address issues
-session_start();
-if (!isset($_SESSION["user"]) || !isset($_SESSION["role"])) {
+// TODO session is not persisting, may be due to localhost or address issues
+
+if (!isset($_SESSION['user']) || !isset($_SESSION['role'])) {
     // If the user is not logged in, redirect to the login page
     header('Location: login.php');
     exit();
@@ -15,9 +15,9 @@ if (!isset($_SESSION["user"]) || !isset($_SESSION["role"])) {
 
 
 
-$username = $_SESSION["user"];
-$role = $_SESSION["role"];
-*/
+$username = $_SESSION['user'];
+$role = $_SESSION['role'];
+
 ?>
 
 <!DOCTYPE html>
@@ -29,7 +29,7 @@ $role = $_SESSION["role"];
 </head>
 <body>
     <div class="welcome-container">
-        <h1>Welcome<?php echo htmlspecialchars($username); ?>!</h1>
+        <h1>Welcome <?php echo htmlspecialchars($username); ?>!</h1>
         <p>You have successfully logged in.</p>
        <!-- <p>Your role is: <?php echo htmlspecialchars($role); ?></p> -->
        <a href="search.php" class="button">Search</a>

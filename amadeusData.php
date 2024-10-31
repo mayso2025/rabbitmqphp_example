15dea3d916c1be9bbf9286b45b34e2c3ead1c4a6
yumis56 <ys56@njit.edu>
2024-10-30 22:50:10 -0400

    FUNCTIONING! still need to remove debug message but committing before something crashes!

--- amadeusData.php
+++ amadeusData.php
@@ -9,7 +9,7 @@ $dotenv->load();
 
 $apiKey = $_ENV['API_KEY'];
 $apiSecret = $_ENV['API_SECRET'];
-$baseUrl = 'http://test.api.amadeus.com/v2';
+$baseUrl = 'https://test.api.amadeus.com/v1';
 
 #echo $apiKey . " " . $apiSecret;
 
@@ -30,34 +30,34 @@ function getAccessToken($apiKey, $apiSecret){
 
 	$response = curl_exec($ch);
 	curl_close($ch);
-
 	$responseData = json_decode($response, true);
-	print_r($responseData);
 	return $responseData['access_token'] ?? null;
 }
 ###
 
 function getFlightOffers($accessToken){
 	global $baseUrl;
-	#$url = $baseUrl . "/shopping/flight-offers";
-	$url = $baseUrl . "/shopping/flight-offers?originLocationCode=NYC&destinationLocationCode=LAX&departureDate=24-11-01&adults=1";
+	#$url = "https://test.api.amadeus.com/v2/shopping/flight-offers";
+	$url = $baseUrl . "/shopping/flight-destinations?origin=PAR";
+	#$url = $baseUrl . "/shopping/flight-offers?originLocationCode=NYC&destinationLocationCode=LAX&departureDate=24-11-01&adults=1";
 
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, [
-		"Authorization: Bearer $accessToken"
+		"Authorization: Bearer $accessToken",
+		
+		#"Content-Type:application/json"
 	]);
 
 	$response = curl_exec($ch);
 	curl_close($ch);
-
 	return json_decode($response,true);
 }
 
 ###
 
 $accessToken = getAccessToken($apiKey, $apiSecret);
-if (isset($accessToken['access_token'])){
+if ($accessToken){
 	$results = getFlightOffers($accessToken);
 	print_r($results);
 } else {

2a683ca8110d181f251ce6d68b1586aeafaaf2bb
pd438 <pd438@njit.edu>
2024-10-31 02:00:52 -0400

    update

--- rate-review.php
+++ rate-review.php
@@ -17,13 +17,14 @@ if (isset($_SESSION['message'])){
         <label for="lname">Review:</label><br>
         <textarea id="box" name="review_box" rows="10" cols="50"> 
         <label for="photo">Upload Photo:</label>
-        <input type="file" name="photo" id="photo" accept="image/*" required><br><br>
-        <button type = "submit"> Submit Review</button>
-        <a href="book.php" class="button">Book a Flight</a>
-        <a href="/Apache/html/logout.php">Logout</a>    
+            
             <!--Got code for textarea from IS117 final project which involed using a textarea, needed a bigger box for reviews-->
             <!--Code edited by Mikey, VSCode was being weird, had someone else do it instead-->
             <!-- Code Revisioned by Paulo Duarte-->
         </textarea>
+        <input type="file" name="photo" id="photo" accept="image/*" required><br><br>
+        <button type = "submit"> Submit Review</button>
+        <a href="book.php" class="button">Book a Flight</a>
+        <a href="/Apache/html/logout.php">Logout</a>
       </form>
 </html>

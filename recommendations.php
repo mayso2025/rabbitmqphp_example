d76c739830884de8d8e83fba633840c29eb74f88
Mike Gabriel Ayson <maa57@njit.edu>
2024-10-31 10:17:28 -0400

    added some test styling to make it look good, not neccesary

--- recommendations.php
+++ recommendations.php
@@ -35,4 +35,22 @@
     </div>
     </div>
     </div>
+    <!-- NOTE!: This is just here for testing purposes to see if the actual styling code works, utilize this in an actual css file (later down the line of course) -->
+    <style> 
+                .card {
+        /* Add shadows to create the "card" effect */
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        transition: 0.3s;
+        }
+
+        /* On mouse-over, add a deeper shadow */
+        .card:hover {
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+        }
+
+        /* Add some padding inside the card container */
+        .container {
+        padding: 2px 16px;
+        }
+    </style>
 </html>

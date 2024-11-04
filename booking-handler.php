d0f45207ad2e73fd9b01c4b308dd451fb2e26914
pd438 <pd438@njit.edu>
2024-11-04 00:36:27 -0500

    changes

--- booking-handler.php
+++ booking-handler.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
 require 'vendor/autoload.php';
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+
+$bookingRequest = array();
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
@@ -11,6 +17,12 @@ $channel = $connection->channel();
 
 // Declare a queue to send messages to
 $channel->queue_declare('booking_queue', false, true, false, false);
+// Database connection inspired from IT202 Fall 2023 with Professor Matthew Toegel. PD438 10/30/2024//
+$dsn = 'mysql:host=localhost;dbname=your_database';
+$username = 'admin';
+$password = '12345';
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+$pdo = new PDO($dsn, $username, $password, $options);
 
 // Collect form data
 $bookingData = [

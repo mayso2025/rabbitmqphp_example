f70ee19e0bd6454929156fecd07950e937bd93dc
pd438 <pd438@njit.edu>
2024-11-05 09:32:27 -0500

    redirect fix

--- welcome.php
+++ welcome.php
@@ -30,7 +30,7 @@ $username = $_SESSION['user'];
         <p>You have successfully logged in.</p>
        <!-- <p>Your role is: <?php echo htmlspecialchars($role); ?></p> -->
        <a href="search.php" class="button">Search</a>
-       <a href="bookinghotel.php" class="button">Book a Hotel</a>
+       <a href="bookingHotel.php" class="button">Book a Hotel</a>
        <a href="bookingFlight.php" class="button">Book a Flight</a>
        <a href="rate-review.php" class="button">Rate&Review</a>
        <a href="/Apache/html/logout.php"class="button">Logout</a>
